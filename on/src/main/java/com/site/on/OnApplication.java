a9f5c7bfcea1b775bb4fb2aeb874f6dd3a8eaf73
Vlad <vladorlovskyy@gmail.com>
2022-07-12 18:39:26 +0300

    Initial commit

---
+++ on/src/main/java/com/site/on/OnApplication.java
@@ -0,0 +1,15 @@
+package com.site.on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OnApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(OnApplication.class, args);
+	}
+
+}
